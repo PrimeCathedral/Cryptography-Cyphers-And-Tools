826e60bde404d95f89290aef85fea9d741e8177e
JimmyCarrero <jimmy.carrero@upr.edu>
2024-12-28 11:33:33 -0400

    this might be to advanced for me right now. I'll come back to it later.

--- Montgomery.cpp
+++ Montgomery.cpp
@@ -18,16 +18,25 @@ Montgomery::cpp_int Montgomery::from_Montgomery(const cpp_int &x) const {
     return (x * R_inv) % modulus;
 }
 
-Montgomery::cpp_int Montgomery::montgomeryReduce(const cpp_int &T) const {
-    cpp_int m {(T * mod_inv) % R};
-    cpp_int t {(T + m * modulus) / R};
 
-    if (t >= modulus) {
-        t -= modulus;
+/**
+ *
+ * @param T a number in the Montgomery space.
+ * @return cpp_int: Montgomery Reduction or REDC output.
+ */
+// https://cp-algorithms.com/algebra/montgomery_multiplication.html#montgomery-reduction
+Montgomery::cpp_int Montgomery::montgomeryReduce(const cpp_int &T) const {
+    cpp_int q {(T % R) * mod_inv % R};
+    cpp_int a {(T - q * modulus) / R};
+
+    if (a < 0) {
+        a += modulus;
     }
 
-    return t;
+    return a;
 }
 Montgomery::cpp_int Montgomery::multiply(const cpp_int &a, const cpp_int &b) const {
-    return montgomeryReduce(a * b);
+    // a * b := a * b * r^-1 mod n
+    return (a * b * R_inv) % modulus;
+    // return montgomeryReduce(a * b);
 }
