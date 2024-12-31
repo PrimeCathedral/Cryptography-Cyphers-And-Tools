b6bdcc87465e48409c5ca06aa84c1642f55140bf
JimmyCarrero <jimmy.carrero@upr.edu>
2024-12-31 13:09:42 -0400

    Added Miller Rabin. Not tested

--- Utilities/Primes.cpp
+++ Utilities/Primes.cpp
@@ -88,3 +88,72 @@ bool Primes::FermatPrimalityTest(const MP::cpp_int& potential_prime, int times) 
     }
     return true;
 }
+
+bool isOdd(const cpp_int& number) {
+    // If the rightmost bit is one, it is odd
+    if (number & 1) return true;
+    return false;
+}
+
+bool isEven(const cpp_int& number) {
+    // If the rightmost bit is one, it is odd
+    if (number & 1) return false;
+    return true;
+}
+
+cpp_int FactorPowersOfTwo(const cpp_int& n) {
+    auto s {0};
+    auto d {n};
+
+    while (isEven(d)) {
+        d >>= 1;
+        s++;
+    }
+    return s;
+}
+
+
+bool Primes::MillerRabinPrimalityTest(const cpp_int& potential_prime, int iterations) {
+
+    // Input n has to be odd and n > 2
+    if (isEven(potential_prime) && potential_prime > 2) return false;
+
+    // Variables and containers used:
+    auto s {0};
+    auto s_copy {};
+    cpp_int d {potential_prime - 1};
+    cpp_int random_a {0};
+    cpp_int x, y;
+
+    // Setup RNG
+    // TODO setup so dist is a global variable with upper limit being changed by each method as necessary
+    const boost::random::uniform_int_distribution<MP::cpp_int> dist(2, potential_prime - 2);
+
+    // Compute n - 1 = 2^s * d
+    while (isEven(d)) {
+        d >>= 1;
+        s++;
+    }
+
+    while (iterations--) {
+        // Generate a random number between [e, p - 2]
+        random_a = dist(gen);
+
+        // Compute a^d % n
+        x = Crypto::ModularArithmetic::modularExponentiation(random_a, d, potential_prime);
+
+        s_copy = s;
+        // For each power of 2 (s)
+        while (s_copy) {
+            // y = x^2 % n
+            y = Crypto::ModularArithmetic::modularExponentiation(x, 2, potential_prime);
+
+            if (y == 1 && x != 1 && x != potential_prime - 1) return false;
+
+            x = y;
+        }
+        if (y != 1) return false;
+    }
+
+    return true;
+}
